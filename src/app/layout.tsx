2413b42376eb29a9aad9846721e52ab61fe57b25
nassim-kada <kadanassim42@gmail.com>
2026-05-08 23:10:04 +0100

    fixing errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Cursor } from "@/components/Cursor";
+
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
